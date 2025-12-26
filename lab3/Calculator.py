d33bb7167b4e46a7cc0dbc36d8055d68cef285d7
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-12-26 12:40:08 +0100

    Uppgift 1 klar

---
+++ lab3/Calculator.py
@@ -0,0 +1,18 @@
+class Calculator():
+    def app():
+        print("Välkommmen till världens bästa kalkylator!")
+
+        tal1 = 9 / 5
+        tal2 = 9 / 5
+        tal3 = 9.0 / 5
+        tal4 = 9.0 / 5.0
+
+        print(f"Tal 1, 9/5 ger: {tal1}")
+        print(f"Tal 2, 9/5 ger: {tal2}")
+        print(f"Tal 3, 9/5 ger: {tal3}")
+        print(f"Tal 4, 9/5 ger: {tal4}")
+        
+    
+    app()
+
+
