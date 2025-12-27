610ee852e5ca79083943b3364c88e1aa04a57781
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-12-27 12:35:43 +0100

    lab4 uppgift 1 klar

--- lab4/Algoritm.py
+++ lab4/Algoritm.py
@@ -1,5 +1,5 @@
 class Algoritm:
-    def app():
+    def uppgift1():
         while True:
             tal1 = input("Ange första talet: ")
             try:
@@ -23,6 +23,33 @@ class Algoritm:
         else:
             print(f"Första talet: {tal1} och andra talet: {tal2} är lika stora")
     
-    app()
+    #Starta uppgift 2 s4 lab4
+    def uppgift2():
+        while True:
+            tal1 = input("Ange första talet: ")
+            try:
+                tal1 = int(tal1) 
+                break
+            except ValueError:
+                print("Fel inmatning")
+
+        while True:
+            tal2 = input("Ange andra talet: ")
+            try:
+                tal2 = int(tal2) 
+                break
+            except ValueError:
+                print("Fel inmatning")
+
+        if tal1 > tal2 and tal1 != tal2:
+           print(f"Tal 1: {tal1} är störst")
+        elif tal1 < tal2 and tal1 != tal2:
+           print(f"Tal 2: {tal2} är störst")
+        else:
+            print(f"Första talet: {tal1} och andra talet: {tal2} är lika stora")
+    
+    
+    #uppgift1()
+    uppgift2()
 
 
