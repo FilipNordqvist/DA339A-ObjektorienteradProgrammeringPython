d00ffd78f9c8d2d714817f1d5ad8694393c8347e
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2026-01-02 15:29:47 +0100

    lab10 klar med 2b

--- lab10/lab10.py
+++ lab10/lab10.py
@@ -1,16 +1,41 @@
 class lab10:
-    def __init__(self,number_to_find):
-        self.number_to_find = number_to_find
-        self.testArray = [32, 27, 64, 18, 95, 14, 90, 70, 60, 37]
+    testArray = [32, 27, 64, 18, 95, 14, 90, 70, 60, 37,95]
 
     def uppgift1(self):
+        number_to_find = 10
         for i in self.testArray:
-            if i == self.number_to_find:
-                print(f"{self.number_to_find} hittades i arrayen")
+            if i == number_to_find:
+                print(f"{number_to_find} hittades i arrayen")
                 return
-        else:
-            print(f"Talet {self.number_to_find} fanns inte i arrayen")
+
+        print(f"Talet {number_to_find} fanns inte i arrayen")
+
+    def uppgift2a(self):
+        biggest_number = max(self.testArray)
+        result = []
+
+        #För att få värde och index behövs enumerate, self.testArray.index(value) ger bara första träffen
+        for index, value in enumerate(self.testArray):
+            if value == biggest_number:
+                result.append([value, index])
+        
+        print("Tal -> Index")
+        for value, index in result:
+            print(value," -> ", index)
+    print()
+
+    def uppgift2b(self):
+        smallest_number = min(self.testArray)
+        result = []
+
+        for index, value in enumerate(self.testArray):
+            if value == smallest_number:
+                result.append([value,index])
+        print("Tal -> Index")
+        for value, index in result:
+            print(value," -> ", index)
+
+
     
-number = int(input("Vilket nummer vill du hitta i arrayen?"))
-lab = lab10(number)
-lab.uppgift1()
+lab = lab10()
+lab.uppgift2b()
