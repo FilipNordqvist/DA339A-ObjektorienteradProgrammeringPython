bc1a50597bca2060aa8449a055681ba9b8dc851c
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2026-01-17 14:37:54 +0100

    La till main

--- lab13/lab13.py
+++ lab13/lab13.py
@@ -1,24 +1,10 @@
 from DogDayCare import DogDayCare
 
-class Lab13():
-    def main(self):
-        dog_day_care = DogDayCare(100)
-        dog_day_care.member_registration()
+
+def main():
+    dog_day_care = DogDayCare(100)
+    dog_day_care.member_registration()
 
 
-Lab13().main()
-    
-
-
-
-        
-
-    
-
-
-
-
-    
-
-   
- 
+if __name__ == "__main__":
+    main()
