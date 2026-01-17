ab011c19ed2c118e4eab27800c0d5a0ec245ba0a
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2026-01-17 14:52:43 +0100

    Kommenterar DDC klassen

--- lab13/DogDayCare.py
+++ lab13/DogDayCare.py
@@ -3,25 +3,33 @@ from Dog import Dog
 
 
 class DogDayCare:
+    """initerar kostnad till 100 kr per hund och skapar plats för en ny hundägare"""
     def __init__(self, cost_for_one_dog):
         self.cost_for_dog = cost_for_one_dog
         self.dog_owner = None
 
+    """Tar input från användaren om vad de heter, deras adress och hur många hundar de har"""
     def member_registration(self):
         owner_name = input("Vad heter du? ")
         address = input("Vad är din adress? ")
         nr_dogs = input("Hur många hundar har du? ")
 
+        """Initerar ett nytt dog_owner objekt med värden från användaren, 
+        self = den aktuella instansen av DogDayCare"""
         self.dog_owner = DogOwner(
             owner_name, address, nr_dogs
-        )  # Skapar dogowner objektet, self betyder att du pratar med samma instans av objektet
-
+        )
+        
+        """Skapar hund-objekt"""
         dog = self.dog_owner.get_dog()
 
+        """Kopplar hund-objektet till ägaren"""
         self.add_dog(dog)
 
+        """Kallar på metoden för att skriva ut info om hund och ägare"""
         self.print_dog_owner_stats()
 
+    """Lägger till hunden till ägaren"""
     def add_dog(self, dog):
         self.dog_owner.dog = dog
 
@@ -37,4 +45,4 @@ class DogDayCare:
         print(dog.get_name(), dog.get_gender(), dog.get_age())
 
     def calculate_cost(self, dog_owner):
-        None
+        pass
--- lab13/lab13.py
+++ lab13/lab13.py
@@ -1,10 +1,13 @@
 from DogDayCare import DogDayCare
 
 
+"""Skapar en instans av DogDayCare och skickar in 100 (pris per hund) 
+till konstruktorn, och anropar sedan member_registration().
+"""
 def main():
     dog_day_care = DogDayCare(100)
     dog_day_care.member_registration()
 
-
+"""Ser till att main() bara körs när filen startas direkt, inte när den importeras."""
 if __name__ == "__main__":
     main()
