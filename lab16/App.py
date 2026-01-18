25dd88cc193ed4378ead705bd504bef95680f122
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2026-01-18 12:46:12 +0100

    Skapar login och app

---
+++ lab16/App.py
@@ -0,0 +1,10 @@
+from Login import Login
+
+
+def main():
+    login_user = Login("homer@simpson.se", "homer2022")
+    login_user.to_string()
+
+
+if __name__ == "__main__":
+    main()
---
+++ lab16/Login.py
@@ -0,0 +1,10 @@
+class Login():
+    def __init__(self,id,password):
+        self.id = id
+        self.password = password
+    
+    def to_string():
+        print(f"ID: {id}, Password: {password}")
+
+    if __name__ == "__main__":
+        to_string()
