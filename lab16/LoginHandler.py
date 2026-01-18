27844bbc8ae0637c554b6e6c3ccf754f54192649
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2026-01-18 14:19:19 +0100

    Listan är klar

---
+++ lab16/LoginHandler.py
@@ -0,0 +1,34 @@
+class LoginHandler:
+    def __init__(self, max_elements):
+        self.login_list = []  # Skapar en tom lista
+        self.num_of_elements = 0  # Startar utan några element
+        self.max_elements = max_elements
+
+    def add_new(self, login):
+        success = True
+
+        # Om login inte är None och antalet element är mindre än max → då går det att lägga till.
+        if login is not None and self.num_of_elements < self.max_elements:
+            self.login_list.append(login)
+            self.num_of_elements += 1
+        else:
+            print("List is full")
+            success == False
+
+        return success
+
+    def remove_elements_at(self,index):
+        success = True
+        
+        if index < 0 or index >= self.num_of_elements:
+            success = False
+            
+        else:
+            self.login_list.pop(index)
+            self.max_elements -= 1
+
+        
+        return success
+
+    def get_numers_of_elements(self):
+        return len(self.login_list)
--- lab16/Main.py
+++ lab16/Main.py
@@ -1,8 +1,58 @@
 from Login import Login
+from LoginHandler import LoginHandler
+
 
 def main():
-    login_user = Login("homer@simpson.se", "homer2022")
-    login_user.to_string()
+    max_elements = 2
+
+    # Initerar ett LoginHandler objekt
+    login_list = LoginHandler(max_elements)
+
+    # Initirerar ett Login objekt och lägger till e-post i listan
+    login1 = Login("homer@simpson.se", "homer2022")
+    ok = login_list.add_new(login1)
+    if ok:
+        print(f"Object added. Num of elements: {login_list.get_numers_of_elements()}")
+    else:
+        print("Object could not be added.")
+
+    # Utskrift för att se listan
+    print("Listan just nu: ")
+    for i, login in enumerate(login_list.login_list):
+        print(f"Index {i}:  {login.id}")
+
+    # Initirerar ett Login objekt och lägger till e-post i listan
+    login2 = Login("filip@email.com", "password")
+    ok = login_list.add_new(login2)
+    if ok:
+        print(f"Object added. Num of elements: {login_list.get_numers_of_elements()}")
+    else:
+        print("Object could not be added.")
+
+    # Utskrift för att se listan
+    print("Listan just nu: ")
+    for i, login in enumerate(login_list.login_list):
+        print(f"Index {i}:  {login.id}")
+
+    # Förösker ta bort ett element som inte finns i listan då den just nu har enbart 2 objekt [0,1]
+    ok = login_list.remove_elements_at(3)
+    if ok:
+        print(f"Object deleted. Num of elements: {login_list.get_numers_of_elements()}")
+    else:
+        print("Object could not be deleted.")
+
+    # Tar bort index 1 från listan
+    ok = login_list.remove_elements_at(1)
+    if ok:
+        print(f"Object deleted. Num of elements: {login_list.get_numers_of_elements()}")
+    else:
+        print("Object could not be deleted.")
+
+    # Utskrift för att se listan
+    print("Listan just nu: ")
+    for i, login in enumerate(login_list.login_list):
+        print(f"Index {i}:  {login.id}")
+
 
 if __name__ == "__main__":
     main()
