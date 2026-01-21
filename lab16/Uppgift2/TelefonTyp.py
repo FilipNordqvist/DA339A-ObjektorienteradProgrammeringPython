f8f5548dbff62a27181a6fd33b14b80c32119876
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2026-01-21 11:17:49 +0100

    Updaterade enum namn

--- lab16/Uppgift2/TelefonTyp.py
+++ lab16/Uppgift2/TelefonTyp.py
@@ -1,8 +1,8 @@
 from enum import Enum
 
 class TelefonTyp(Enum):
-    privat = 0
-    arbete = 1
+    ARBETE = "arbete"
+    HEM = "hem"
     
     def __str__(self):
         return f"Telefon typ: {self.name}"
