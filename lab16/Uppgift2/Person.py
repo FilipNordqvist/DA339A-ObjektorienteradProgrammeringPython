ab59bfee3e0c275c8dc0288f98cf4971442e60dc
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2026-01-21 11:15:23 +0100

    La till getter och setter

--- lab16/Uppgift2/Person.py
+++ lab16/Uppgift2/Person.py
@@ -1,12 +1,21 @@
 from Telefon import Telefon
 
-class Person():
-    
-    def __init__(self,nummer):
-        self.nummer = nummer
-        
-    def get_nummer(self):
-        return self.nummer
-    
-    def set_nummer(self, nummer):
-        self.nummer = nummer
+class Person:
+    def __init__(self, namn, telefon):
+        self.namn = namn
+        self.telefon = telefon
+
+    def get_namn(self):
+        return self.namn
+
+    def get_telefon(self):
+        return self.telefon
+
+    def set_namn(self, namn):
+        self.namn = namn
+
+    def set_telefon(self, telefon):
+        self.telefon = telefon
+
+    def __str__(self):
+        return self.namn + " har numret " + str(self.telefon)
