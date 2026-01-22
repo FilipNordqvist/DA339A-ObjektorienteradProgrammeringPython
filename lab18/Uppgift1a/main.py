d58f2a66118c74886298272b33a383a882b62d9f
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2026-01-22 14:39:49 +0100

    Grundkod ska läggas i MVC

--- lab18/Uppgift1a/main.py
+++ lab18/Uppgift1a/main.py
@@ -12,7 +12,26 @@ def user__difficulty():
 
 
 def game(difficulty):
-    print(f"Skriver ut i game --> {difficulty}")
+    winning_number = random.randrange(0,difficulty)
+    number_of_guesses = 0
+    
+    while True:
+        try:
+            guess = int(input(f"Try to guess a number between 0-{difficulty}: "))
+        
+        except ValueError:
+            print("Please enter a valid number.")
+            continue
+        
+        if guess < winning_number:
+            print(f"You guessed to low, guess again {guess}")
+            number_of_guesses += 1
+        elif guess > winning_number:
+            print(f"You guessed to high, guess again {guess}")
+            number_of_guesses += 1
+        else:
+            print(f"It took you {number_of_guesses} moves to guess {winning_number}")
+            break
 
 
 if __name__ == "__main__":
