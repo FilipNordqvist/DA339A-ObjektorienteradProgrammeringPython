25cfb6d7cf0bb345be398045564d7e19ea9d7f32
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2026-01-22 14:03:37 +0100

    Testa property

--- Test/main.py
+++ Test/main.py
@@ -1,13 +1,45 @@
-from Person import Person
+class Rectangel:
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+        
+    @property
+    def width(self):
+        return f"{self._width:.1f}cm"
+    
+    @width.setter
+    def width(self, new_width):
+        if new_width > 0:
+            self._width = new_width
+        else:
+            print("Width must be greater then zero")
+    
+    @property
+    def height(self):
+        return f"{self._height:.1f}cm"
+    
+    @height.setter
+    def height(self,new_height):
+        if new_height > 0:
+            self._height = new_height
+        else:
+            print("Height must be greater then zero")
+            
+    @width.deleter
+    def width(self):
+        del self._width
+        print("width deleted")
+        
+    @height.deleter
+    def height(self):
+        del self._height
+        print("height deleted")
 
 
-def main():
-    person = Person("Kalle",12)
-    
-    print(person.name)
-    person.name = "Filip"
-    print(person.name)
-    
-if __name__ == "__main__":
-    main()
-    
+rectangel = Rectangel(3, 4)
+
+rectangel.width = 5
+rectangel.height = 6
+
+del rectangel.width
+del rectangel.height
